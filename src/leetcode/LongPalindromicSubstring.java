25ff31806b2ab690fc34d59ddb94651577e157f0
donald <realdonald1994@outlook.com>
2019-07-05 10:38:48 -0400

    done

--- src/leetcode/LongPalindromicSubstring.java
+++ src/leetcode/LongPalindromicSubstring.java
@@ -1,7 +1,37 @@
 package leetcode;
 
 public class LongPalindromicSubstring {
+	
 	public static void main(String[] args) {
+		
+		String str = new Solution().longPalStr("cbbd");
+		System.out.println(str);
+	}
+	
+	
+}
+ class Solution{
+	String out = "";
+	public  String longPalStr(String s) {
+		
+		for(int i =0;i<s.length();i++) {
+			compute(s,i,0);
+			compute(s,i,1);
+		}
+		return out;
+		
+	}
+	public  void compute(String s,int left,int offset) {
+		int right = left+offset;
+		while(left>=0&&right<s.length()&&s.charAt(left)==s.charAt(right)) {
+			left--;
+			right++;
+		}
+		int begin = left+1;
+		String cur = s.substring(begin, right);
+		if(cur.length()>out.length()) {
+			out= cur;
+		}
 		
 	}
 }
